f6b4eef6bef1f999e3fb4584739f040cc2c75cc6
roylaurie <roy.laurie.pub@asmov.software>
2024-01-20 17:00:54 -0800

    attrval

--- app/web/src/main.rs
+++ app/web/src/main.rs
@@ -10,9 +10,15 @@ fn App() -> Html {
         AttrValue::Static("23001"),
     ];
 
+    let log: Vec<AttrValue> = vec![
+        AttrValue::Static("lorem ipsum lorem ipsum lorem ipsum"),
+        AttrValue::Static("ipsum lorem ipsum lorem ipsum lorem ipsum"),
+        AttrValue::Static("lorem ipsum lorem ipsum lorem ipsum lorem ipsum")
+    ];
+
     html! {
         <div id="app" class="container h-screen mx-auto p-1">
-            <Terminal title="This is a title" stats={stats} />
+            <Terminal title="This is a title" log={log} stats={stats} />
         </div>
     }
 }
@@ -20,22 +26,19 @@ fn App() -> Html {
 #[derive(PartialEq, Properties)]
 struct TerminalProps {
     title: AttrValue,
-    stats: Vec<AttrValue>
+    stats: Vec<AttrValue>,
+    log: Vec<AttrValue>
 }
 
 #[function_component]
 fn Terminal(props: &TerminalProps) -> Html {
-    let mut log: Vec<String> = Vec::new();
-    log.push("lorem ipsum lorem ipsum lorem ipsum".to_string());
-    log.push("ipsum lorem ipsum lorem ipsum lorem ipsum".to_string());
-    log.push("Hello".to_string());
     html! {
         <div id="terminal" class="border rounded p-1 h-full flex flex-col space-y-2">
             <div id="titlebar" class="border rounded px-2 text-center text-lg">
                 <span>{props.title.clone()}</span>
             </div>
             <div id="lines" class="h-full border rounded p-1 overflow-y-scroll">
-                <p>{log.iter().map(|s| html!{<p>{s}</p>}).collect::<Html>()}</p>
+                {props.log.iter().map(|s| html!{<p>{s}</p>}).collect::<Html>()}
             </div>
             <input type="input" class="hover:shadow-md rounded border px-4" placeholder="input text ..." title="Input" />
             <div id="footer" class={classes!("flex", "justify-between", "items-center", "px-6", "text-sm", "*:border", "*:rounded", "*:px-1")}>
